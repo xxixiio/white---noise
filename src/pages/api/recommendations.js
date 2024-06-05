c3ca8cf47f7a5f5f90f6a3617cd0e2de9f5e3753
hizer00 <bastixvii@gmail.com>
2024-06-05 22:17:37 +0200

    feat(api): USELESS. track recommendations endpoint

---
+++ src/pages/api/recommendations.js
@@ -0,0 +1,19 @@
+import { client } from "../../utils/apiConnection";
+
+export const GET = async ({ params, request }) => {
+  const url = new URL(request.url);
+  const query = new URLSearchParams(url.search);
+
+  const queryData = {
+    artist: query.get("artist"),
+    tracks: query.get("tracks")
+  };
+
+  const options = {
+    limit: 5,
+    seed_artists: queryData.artist,
+    seed_tracks: queryData.tracks
+  };
+  const recommendations = await client.browse.getRecommendations(options);
+  return new Response(JSON.stringify(recommendations));
+};
