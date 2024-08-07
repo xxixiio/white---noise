84da884e577611f4c57099fb43bf73a1f838618f
hizer00 <bastixvii@gmail.com>
2024-08-07 20:50:21 +0200

    refactor: complete rework on front page

--- src/pages/api/albums/get-new-releases.js
+++ src/pages/api/albums/get-new-releases.js
@@ -1,6 +1,6 @@
 import { client } from "../../../utils/apiConnection";
 
 export const GET = async ({ params, request }) => {
-  const newReleasesAlbums = await client.browse.getNewReleases({ limit: 15 });
+  const newReleasesAlbums = await client.browse.getNewReleases({ limit: 10 });
   return new Response(JSON.stringify(newReleasesAlbums));
 };  
